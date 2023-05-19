e9af36f73f1fea48d0b253d7dcce2bcfc5c8f696
maxsvst <fioklin2015@mail.ru>
2023-05-19 21:09:14 +0300

    Страница просмотра РПД

---
+++ src/containers/show-rpd/ShowRpd.js
@@ -0,0 +1,15 @@
+import React, { useRef, useEffect } from "react";
+
+import WebViewer from "@pdftron/webviewer";
+
+export default function ShowRpd() {
+  return (
+    <div>
+      <div>Просмотр Тем</div>
+      <div>Просмотр ЛР</div>
+      <div>Просмотр ПЗ</div>
+      <div>Просмотр Компетенций</div>
+      <div>Просмотр Лекций</div>
+    </div>
+  );
+}
