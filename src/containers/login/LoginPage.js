ab2266241c820c4b5396251fc1b127fede7bb535
maxsvst <fioklin2015@mail.ru>
2023-06-14 14:08:40 +0300

    Стилизация страниц авторизации и регистрации

--- src/containers/login/LoginPage.js
+++ src/containers/login/LoginPage.js
@@ -4,6 +4,8 @@ import { useForm } from "react-hook-form";
 import { eye } from "react-icons-kit/feather/eye";
 import { eyeOff } from "react-icons-kit/feather/eyeOff";
 
+import "./login.css";
+
 import Input from "../../blocks/input/Input";
 import PasswordInput from "../../blocks/passwordInput/PasswordInput";
 import OutlinedButton from "../../blocks/outlined-button/OutlinedButton";
@@ -39,62 +41,66 @@ export default function LoginPage() {
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
+    toRpdDiscipline();
   };
 
   return (
-    <div className="">
-      <div className="">
-        <form className="" onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            register={register("email", {
-              required: "Поле e-mail обязательно к заполнению",
-              pattern: {
-                value: /()/,
-                message: "Введён неверный e-mail",
-              },
-            })}
-            id="email"
-            label="Электронная почта"
-          />
-          {errors.email && <span className="">{errors.email.message}</span>}
-          <PasswordInput
-            register={register("password", {
-              required: "Поле пароля обязательно к заполению",
-              minLength: {
-                value: 8,
-                message: "Пароль должен содержать больше 8-ми символов",
-              },
-              maxLength: {
-                value: 20,
-                message: "Пароль должен содержать меньше 20-ти символов",
-              },
-              pattern: {
-                value: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})/,
-                message:
-                  "Пароль должен содержать цифры, а также буквы верхнего и нижнего регистров, и специальные символы",
-              },
-            })}
-            type={type}
-            id="password"
-            label="Пароль"
-            icon={icon}
-            toggleHandler={toggleHandler}
-          />
-          {errors.password && (
-            <span className="">{errors.empasswordail.message}</span>
-          )}
-          <OutlinedButton
-            type="submit"
-            handleClick={toRpdDiscipline}
-            text="Авторизоваться"
-          />
-          <OutlinedButton
-            handleClick={toRigestration}
-            text="Ещё нет аккаунта?"
-          />
-        </form>
+    <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+      <span className="login-form__title">Авторизация</span>
+      <div className="login-form__password-input-component">
+        <Input
+          register={register("email", {
+            required: "Поле e-mail обязательно к заполнению",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+              message: "Введён неверный e-mail",
+            },
+          })}
+          id="email"
+          label="Электронная почта"
+        />
+        {errors.email ? (
+          <span className="login-form_errors">
+            {errors.email ? errors.email.message : "Введите e-mail"}
+          </span>
+        ) : (
+          <span className="login-form__helper-text">Введите e-mail</span>
+        )}
       </div>
-    </div>
+      <div className="login-form__password-input-component">
+        <PasswordInput
+          register={register("password", {
+            required: "Поле пароля обязательно к заполению",
+            minLength: {
+              value: 8,
+              message: "Пароль должен содержать больше 8-ми символов",
+            },
+            maxLength: {
+              value: 20,
+              message: "Пароль должен содержать меньше 20-ти символов",
+            },
+            pattern: {
+              value: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})/,
+              message:
+                "Пароль должен содержать цифры, а также буквы верхнего и нижнего регистров, и специальные символы",
+            },
+          })}
+          type={type}
+          id="password"
+          label="Пароль"
+          icon={icon}
+          toggleHandler={toggleHandler}
+        />
+        {errors.password ? (
+          <span className="login-form_errors">
+            {errors.password ? errors.password.message : "Введите e-mail"}
+          </span>
+        ) : (
+          <span className="login-form__helper-text">Введите пароль</span>
+        )}
+      </div>
+      <OutlinedButton type="submit" handleClick={handleSubmit} text="Войти" />
+      <OutlinedButton handleClick={toRigestration} text="Ещё нет аккаунта?" />
+    </form>
   );
 }
--- src/containers/registration/RegistrationPage.js
+++ src/containers/registration/RegistrationPage.js
@@ -4,6 +4,8 @@ import { eyeOff } from "react-icons-kit/feather/eyeOff";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
+import "./registration.css";
+
 import Input from "../../blocks/input/Input";
 import PasswordInput from "../../blocks/passwordInput/PasswordInput";
 import OutlinedButton from "../../blocks/outlined-button/OutlinedButton";
@@ -52,86 +54,107 @@ export default function RegistrationPage() {
   };
 
   return (
-    <div>
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Input
-            register={register("email", {
-              required: "Поле e-mail обязательно к заполнению",
-              pattern: {
-                value: /()/,
-                message: "Введён неверный e-mail",
-              },
-            })}
-            id="email"
-            label="Электронная почта"
-          />
-          {errors.email && <span className="">{errors.email.message}</span>}
-          <PasswordInput
-            register={register("password", {
-              required: "Поле пароля обязательно к заполению",
-              minLength: {
-                value: 8,
-                message: "Пароль должен содержать больше 8-ми символов",
-              },
-              maxLength: {
-                value: 20,
-                message: "Пароль должен содержать меньше 20-ти символов",
-              },
-              pattern: {
-                value: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})/,
-                message:
-                  "Пароль должен содержать цифры, а также буквы верхнего и нижнего регистров, и специальные символы",
-              },
-            })}
-            type={passwordType}
-            id="password"
-            label="Пароль"
-            icon={passwordIcon}
-            toggleHandler={passwordToggleHandler}
-          />
-          {errors.password && (
-            <span className="">{errors.password.message}</span>
-          )}
-          <PasswordInput
-            register={register("repeatPassword", {
-              required: "Поле повторения пароля обязательно к заполению",
-              minLength: {
-                value: 8,
-                message: "Пароль должен содержать больше 8-ми символов",
-              },
-              maxLength: {
-                value: 20,
-                message: "Пароль должен содержать меньше 20-ти символов",
-              },
-              pattern: {
-                value: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})/,
-                message:
-                  "Пароль должен содержать цифры, а также буквы верхнего и нижнего регистров, и специальные символы",
-              },
-              validate: (vaule) => {
-                if (watch("password") !== vaule) {
-                  return "Пароли не совпадают";
-                }
-              },
-            })}
-            type={repeatPasswordType}
-            id="repeatPassword"
-            label="Повторите пароль"
-            icon={repeatPasswordIcon}
-            toggleHandler={repeatPasswordToggleHandler}
-          />
-          {errors.repeatPassword && (
-            <span className="">{errors.repeatPassword.message}</span>
-          )}
-          <OutlinedButton
-            type="submit"
-            handleClick={toLogin}
-            text="Зарегистрироваться"
-          />
-          <OutlinedButton handleClick={toLogin} text="Уже етсь аккаунт?" />
-        </form>
+    <form class="registration-form" onSubmit={handleSubmit(onSubmit)}>
+      <span className="registration-form__title">Регистрация</span>
+      <div className="registration-form__password-input-component">
+        <Input
+          register={register("email", {
+            required: "Поле e-mail обязательно к заполнению",
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+              message: "Введён неверный e-mail",
+            },
+          })}
+          id="email"
+          label="Электронная почта"
+        />
+        {errors.email ? (
+          <span className="registration-form_errors">
+            {errors.email ? errors.email.message : "Введите e-mail"}
+          </span>
+        ) : (
+          <span className="registration-form__helper-text">Введите e-mail</span>
+        )}
       </div>
-    </div>
+      <div className="registration-form__password-input-component">
+        <PasswordInput
+          register={register("password", {
+            required: "Поле пароля обязательно к заполению",
+            minLength: {
+              value: 8,
+              message: "Пароль должен содержать больше 8-ми символов",
+            },
+            maxLength: {
+              value: 20,
+              message: "Пароль должен содержать меньше 20-ти символов",
+            },
+            pattern: {
+              value: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})/,
+              message:
+                "Пароль должен содержать цифры, а также буквы верхнего и нижнего регистров, и специальные символы",
+            },
+          })}
+          type={passwordType}
+          id="password"
+          label="Пароль"
+          icon={passwordIcon}
+          toggleHandler={passwordToggleHandler}
+        />
+        {errors.password ? (
+          <span className="registration-form_errors">
+            {errors.password ? errors.password.message : "Введите e-mail"}
+          </span>
+        ) : (
+          <span className="registration-form__helper-text">Введите пароль</span>
+        )}
+      </div>
+      <div className="registration-form__password-input-component">
+        <PasswordInput
+          register={register("repeatPassword", {
+            required: "Поле повторения пароля обязательно к заполению",
+            minLength: {
+              value: 8,
+              message: "Пароль должен содержать больше 8-ми символов",
+            },
+            maxLength: {
+              value: 20,
+              message: "Пароль должен содержать меньше 20-ти символов",
+            },
+            pattern: {
+              value: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})/,
+              message:
+                "Пароль должен содержать цифры, а также буквы верхнего и нижнего регистров, и специальные символы",
+            },
+            validate: (vaule) => {
+              if (watch("password") !== vaule) {
+                return "Пароли не совпадают";
+              }
+            },
+          })}
+          type={repeatPasswordType}
+          id="repeatPassword"
+          label="Повторите пароль"
+          icon={repeatPasswordIcon}
+          toggleHandler={repeatPasswordToggleHandler}
+        />
+        {errors.repeatPassword ? (
+          <span className="registration-form_errors">
+            {errors.repeatPassword
+              ? errors.repeatPassword.message
+              : "Введите e-mail"}
+          </span>
+        ) : (
+          <span className="registration-form__helper-text">
+            Повторите пароль
+          </span>
+        )}
+      </div>
+      <OutlinedButton
+        type="submit"
+        handleClick={handleSubmit}
+        text="Зарегистрироваться"
+      />
+      <OutlinedButton handleClick={toLogin} text="Уже есть аккаунт?" />
+    </form>
   );
 }
